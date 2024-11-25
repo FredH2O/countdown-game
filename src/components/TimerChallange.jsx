4f68411ea541de2c2f5b05ad4556a19c29ecbd0f
Fred <huertaserick@yahoo.ie>
2024-11-25 14:09:37 +0000

    use portal practice

--- src/components/TimerChallange.jsx
+++ src/components/TimerChallange.jsx
@@ -10,8 +10,12 @@ export default function TimerChallange({ targetTime, title }) {
 
   if (timeRemaining <= 0) {
     clearInterval(timer.current);
-    setTimeRemaining(targetTime * 1000);
+
     dialog.current.open();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
   }
 
   function handleStart() {
@@ -27,7 +31,12 @@ export default function TimerChallange({ targetTime, title }) {
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
